b72d79218cd6fb6b8470143fbb9b6968bd5619e3
Mohammed <mdjahangirpasha.adf@gmail.com>
2019-06-16 12:46:27 +0300

    Added Servlet and all

---
+++ springMVCDemo1/springmvcdemo1/src/main/resources/ipa/edu/sa/AddController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ipa.edu.sa;
+
+/**
+ * @author PASHAM
+ *
+ */
+
+public class AddController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
