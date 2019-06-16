379ca33a8a61e45ef95393e30900e4519d8f9452
Mohammed <mdjahangirpasha.adf@gmail.com>
2019-06-16 12:52:46 +0300

    Added

---
+++ springMVCDemo1/springmvcdemo1/src/main/java/ipa/edu/sa/AddController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ipa.edu.sa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author PASHAM
+ *
+ */
+@Controller
+public class AddController {
+	
+	@RequestMapping("/add")
+	public void add() {
+		System.out.print("I am here:::");
+		
+		
+		
+	}
+
+}
